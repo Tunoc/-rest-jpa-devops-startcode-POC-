c1de24cf53dcaa175ca00f283e5e96a58f0110d0
Tunoc <random959dude@gmail.com>
2020-02-11 08:22:11 +0100

    .

--- src/main/java/facades/MovieFacade.java
+++ src/main/java/facades/MovieFacade.java
@@ -1,10 +1,8 @@
 package facades;
 
 import entities.Movie;
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 /**
  *
