facc25612722be71f076998a76a8a6bb50e02775
Tunoc <random959dude@gmail.com>
2020-02-11 08:13:13 +0100

    .

---
+++ src/main/java/facades/MovieFacade.java
@@ -0,0 +1,72 @@
+package facades;
+
+import entities.Movie;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class MovieFacade {
+
+    private static MovieFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private MovieFacade() {
+    }
+
+    /**
+     *
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static MovieFacade getMovieFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MovieFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    //CRUD
+    public Movie createMovie(Long id, int year, String name, String[] actors) {
+        Movie movie = new Movie(id, year, name, actors);
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(movie);
+            em.getTransaction().commit();
+            return movie;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Movie getMovie(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Movie movie = em.find(Movie.class, id);
+            return movie;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public void populateDB() {
+        EntityManager em = emf.createEntityManager();
+        //Add Movie
+        String[] actors = {"Actor 1", "And actor 2"};
+        Movie e1 = createMovie(1L, 2000, "ABC", actors);
+        Movie e2 = createMovie(2L, 2001, "DEF", actors);
+        Movie e3 = createMovie(3L, 2002, "GHI", actors);
+    }
+
+}
--- src/main/java/rest/MovieResource.java
+++ src/main/java/rest/MovieResource.java
@@ -2,14 +2,12 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import entities.RenameMe;
+import entities.Movie;
 import utils.EMF_Creator;
-import facades.FacadeExample;
+import facades.MovieFacade;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.Consumes;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,34 +15,41 @@ import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
 @Path("xxx")
-public class RenameMeResource {
+public class MovieResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
-                "pu",
-                "jdbc:mysql://localhost:3307/startcode",
-                "dev",
-                "ax2",
-                EMF_Creator.Strategy.CREATE);
-    
+            "pu",
+            "jdbc:mysql://localhost:3307/movieDB",
+            "dev",
+            "ax2",
+            EMF_Creator.Strategy.CREATE);
+
     //An alternative way to get the EntityManagerFactory, whithout having to type the details all over the code
     //EMF = EMF_Creator.createEntityManagerFactory(DbSelector.DEV, Strategy.CREATE);
-    
-    private static final FacadeExample FACADE =  FacadeExample.getFacadeExample(EMF);
+    private static final MovieFacade FACADE = MovieFacade.getMovieFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-            
+
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String demo() {
         return "{\"msg\":\"Hello World\"}";
     }
-    @Path("count")
+
     @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public String getRenameMeCount() {
-        long count = FACADE.getRenameMeCount();
-        //System.out.println("--------------->"+count);
-        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson1(@PathParam("id") Long id) {
+        Movie movie = FACADE.getMovie(id);
+        return new Gson().toJson(movie);
+    }
+    
+    @GET
+    @Path("/populate/database")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson5() {
+        FACADE.populateDB();
+        String msg = "Updated DB";
+        return new Gson().toJson(msg);
     }
 
- 
 }
